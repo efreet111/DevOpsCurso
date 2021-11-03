6b26ea39280698221d9107188453434becb21752
Victor Silgado <vsilgado@thefactoryhka.com>
2021-11-03 08:46:52 -0500

    tercer commit

---
+++ Stacks/Stacks.Test/StacksTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Stacks;
+
+namespace Stacks.Test
+{
+    [TestFixture]
+    public class StacksTests
+    {
+        
+        [Test]
+        public void Push_Argumento_Nulo_ArrojaExcepcion()
+        {
+            var stack = new Stacks<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_Argumento_Valido()
+        {
+            var stack = new Stacks<string>();
+
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+
+            
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stacks<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+    }
+}
